fa0cb1534b0b64dddabfc44d5bfeaf552548412e
valariembithe <valmbithe691@gmail.com>
2024-03-18 03:44:53 -0700

    Complete tasks

---
+++ utils/redis.js
@@ -0,0 +1,34 @@
+import { promisify } from "utils";
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (err) => {
+      console.error('Redis client failed to connect:', err.toString() || err.message);
+      this.isClientConnected = false;
+    });
+    this.client.on('connect', () => {
+      this.isClientConnected = true;
+    });
+    
+  }
+  isAlive() {
+    return this.isClientConnected;
+  }
+
+  async get(key) {
+    return promisify(this.client.GET).bind(this.client)(key);
+  }
+
+  async set(key, value, duration) {
+    await promisify(this.client.SETEX).bind(this.client)(key, duration, value);
+    }
+  async del(key) {
+    await promisify(this.client.DEL).bind(this.client)(key);
+  }
+}
+
+
+export default redisClient = new RedisClient();
+export default redisClient;
