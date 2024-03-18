a36d2e5fa040f181f8949c1f623da139e876ad3e
valariembithe <valmbithe691@gmail.com>
2024-03-18 03:52:49 -0700

    Database

---
+++ utils/db.js
@@ -0,0 +1,31 @@
+import mongodb from 'mongodb';
+import Collection from 'mongodb/lib/collection';
+import envLoader from './env_loader';
+
+class DBClient {
+  constructor() {
+    env_loader();
+    const host = process.env.DB_HOST || localhost;
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || files_manager;
+    const db_url = `mongodb://${host}:${port}/${database}`;
+
+    this.client = new mongodb.MongoClient(db_url, {useUnifiedTopology: true});
+    this.client.connect();
+  }
+
+  isAlive {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    return this.client.db().collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.client.db().collection('files').countDocuments();
+  }
+}
+
+export default dbClient = new DBClient;
+export default dbClient;
