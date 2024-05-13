0861a165bac1bd10f317c739f50e11dd2ff4d9d4
valariembithe <valmbithe691@gmail.com>
2024-05-13 15:53:38 +0300

    Task 0

--- utils/redis.js
+++ utils/redis.js
@@ -1,4 +1,4 @@
-import { promisify } from '';
+import { promisify } from 'util';
 import { RedisClient } from "redis";
 
 class RedisClient {
@@ -17,7 +17,21 @@ class RedisClient {
 
   isAlive() {
     return this.isClientConnected;
-  };
+  }
 
-  async get
+  async get(key) {
+    return promisify(this.client.GET).bind(this.client)(key);
+  }
+
+  async set(key, value, time) {
+    return promisify(this.client.SETTEX)
+      .bind(this.client)(key, time, value);
+  }
+
+  async del(key) {
+    return promisify(this.client.DEL).bind(this.client)(key);
+  }
 }
+
+export const redisClient = new RedisClient();
+export default redisClient;
