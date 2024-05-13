5f3a69f1f01ca0c19b8d0db15d70c39a81592256
valariembithe <valmbithe691@gmail.com>
2024-05-13 17:31:12 +0300

    Task 0

--- utils/redis.js
+++ utils/redis.js
@@ -14,7 +14,60 @@ class RedisClient {
     this.client.on('error', (err) => {
       console.error('Redis client failed to connect:', err.message || err.toString());
       this.isClientConnected = false;
-    });
+    });const redis = require('redis');
+
+    class RedisClient {
+      constructor() {
+        this.client = redis.createClient();
+    
+        this.client.on('error', (error) => {
+          console.error('Redis Error:', error);
+        });
+      }
+    
+      isAlive() {
+        return this.client.connected;
+      }
+    
+      async get(key) {
+        return new Promise((resolve, reject) => {
+          this.client.get(key, (error, value) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(value);
+      }
+          });
+        });
+      }
+    
+      async set(key, value, duration) {
+        return new Promise((resolve, reject) => {
+          this.client.set(key, value, 'EX', duration, (error, result) => {
+      if (error) {
+              reject(error);
+      } else {
+        resolve(result);
+      }
+          });
+        });
+      }
+    
+      async del(key) {
+        return new Promise((resolve, reject) => {
+          this.client.del(key, (error, result) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(result);
+      }
+          });
+        });
+      }
+    }
+    
+    const redisClient = new RedisClient();
+    module.exports = redisClient;
     this.client.on('connect', () => {
       this.isClientConnected = true;
     });
